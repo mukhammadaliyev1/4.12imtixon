fe71db523c4e1163496c008acbfea5fa0a49a267
mukhammadaliyev1 <muhammadnurmamadaliyev335@gmail.com>
2024-06-29 10:09:52 +0500

    a

---
+++ index.js
@@ -0,0 +1,133 @@
+// 1-misol
+
+// const str = '34567'
+// function reverseStr(str){
+//     let res=str.split('')
+//     return res.reverse()
+// }
+// console.log(reverseStr(str));
+
+// 2-misol
+
+// == -- bu if ning ichida elementlarni tekshirishimizda foydalanamiz misol uchun berilgan son juftmi deb soralganda 2ta tenglikdan
+// // Stringlarda yani malum bir stringda misol uchun 'a'harfi necha marta qaatnashgan degan joylarida ishlatamiz
+
+// 3-misol
+
+
+// function newarr(arr){
+// let arr1=[]
+//     for(let i=1;i<=arr;i++){
+//         arr1.push(i)
+//     }
+//     return arr1
+        
+// }console.log(newarr(10));
+
+
+// 4-misol
+
+// let arr=[1,2,3,4,5,6,7,8,9]
+
+// function sumAll(arr){
+//     let sum1=0;
+//     let sum2=0;
+//     let ar1=[]
+
+//     arr.forEach(function(value){
+//         if(value%2==0){
+//             sum1+=value
+            
+//         }if(value%2==1){
+//             sum2+=value
+//         }
+
+//     })
+// return  
+
+// }
+// console.log(sumAll(arr));
+
+
+// 5-misol
+
+
+// const str=" s!a!l!o!m"
+// function deletesym(str){
+//     return str.replaceAll('!','')
+// }
+// console.log(deletesym(str));
+
+// 6-misol
+
+// let arr1=[1,2,3,4]
+// let arr2=[1,2,3,4]
+// function sumAllarr(arr1,arr2){
+//     let arr=[]
+//     let sum1=0
+//     let sum2=0
+//     let sumAll=0
+  
+// arr1.forEach(function(value){
+//     return sum1+=value
+// })
+// arr2.forEach(function(value){
+//     return sum2+=value
+// })
+// return sumAll=sum1+sum2
+    
+// }console.log(sumAllarr(arr1,arr2));
+
+
+// 7-misol
+
+// const str= 'salom'
+
+// function deleteFirstandlastelement(str){
+   
+
+// }
+
+
+
+// 8-misol
+
+
+// let arr=[1,2,3,4,5,6,7,8,9]
+// function teskarisigachiqarish(arr) {
+//     let reversearray = [];
+//     for (let i = arr.length - 1; i >= 0; i--) {
+//         reversearray.push(arr[i]);
+//     }
+//     return reversearray;
+// } 
+// console.log(teskarisigachiqarish(arr));
+
+
+// 9-misol
+
+// const str ='122330';
+// function removeZero(str){
+//     return str.replace(/0+$/, '');
+
+// }
+// console.log(removeZero(str));
+
+
+
+// 10-misol
+
+
+// let arr = [5,2,3,4,5]
+
+// function findsmallelement(arr){
+// let min=0;
+// arr.forEach(function(value){
+//   if(value<min){
+//     min=value
+//   }
+// })
+// return min;
+// }
+// console.log(findsmallelement(arr));
+
